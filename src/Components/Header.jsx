4193636607e54469a20748d4b9f232450676ae39
Luis Jorge <gevolutionxalapa@gmail.com>
2021-07-05 09:14:27 -0500

    Component style practica de como instalarlo y configurarlo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,26 @@
 import React, {Fragment} from 'react'
+import styled from '@emotion/styled'
+
+const ContenedorHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`
+
+const TextHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`
 
 const Header = ({titulo}) => {
     return (
         <Fragment>
-            <header>
-                <h1>{titulo}</h1>
-            </header>
+            <ContenedorHeader>
+                <TextHeader>{titulo}</TextHeader>
+            </ContenedorHeader>
         </Fragment>
     )
 }
